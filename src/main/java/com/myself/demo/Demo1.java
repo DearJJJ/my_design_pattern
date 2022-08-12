c6d3a527173fdb0163150db405fca3ec402d752b
liaoqingsheng <2677388021@qq.com>
2022-08-12 23:39:02 +0800

    提交一下

--- src/main/java/com/myself/demo/Demo1.java
+++ src/main/java/com/myself/demo/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("我进行了修改，没有本地提交，小心被删");
         System.out.println("hello world");
+        System.out.println("我进行了修改，没有本地提交，小心被删");
     }
 }
