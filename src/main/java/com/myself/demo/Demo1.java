4ed2fd861ab40e8fca894b4bcf6bb387da17394d
liaoqingsheng <2677388021@qq.com>
2022-08-12 23:43:32 +0800

    我进行了修改，并提交，推送

--- src/main/java/com/myself/demo/Demo1.java
+++ src/main/java/com/myself/demo/Demo1.java
@@ -5,5 +5,7 @@ public class Demo1 {
         System.out.println("我进行了修改，没有本地提交，小心被删");
         System.out.println("hello world");
         System.out.println("我进行了修改，没有本地提交，小心被删");
+
+        System.out.println("我进行了修改，并提交");
     }
 }
