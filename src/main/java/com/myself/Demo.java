3e46b61fd5fbb1d895d9bd1e54240da31b39fc6a
liaoqingsheng <2677388021@qq.com>
2022-08-12 22:14:44 +0800

    创建第一个提交

--- src/main/java/com/myself/Demo.java
+++ src/main/java/com/myself/Demo.java
@@ -3,5 +3,6 @@ package com.myself;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(3 / 5);
+        System.out.println("This is a new git commit");
     }
 }
