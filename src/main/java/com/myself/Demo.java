570fd4e83d81e80bdbd2ff480e7bde2549d47ea7
liaoqingsheng <2677388021@qq.com>
2022-08-12 22:17:24 +0800

    拉代码解决了冲突，并push

--- src/main/java/com/myself/Demo.java
+++ src/main/java/com/myself/Demo.java
@@ -3,6 +3,7 @@ package com.myself;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(3 / 5);
+        System.out.println("I also a new commit");
         System.out.println("This is a new git commit");
     }
 }
