1d38af003f37dceca5f780bb321ec97128e5b5c2
liaoqingsheng <2677388021@qq.com>
2022-08-12 22:56:07 +0800

    提交一下，不推送

---
+++ src/test/java/com/myself/multithread/ConcurrentTest.java
@@ -0,0 +1,31 @@
+package com.myself.multithread;
+
+public class ConcurrentTest {
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws Exception {
+
+        //serial();
+        //concurrency();
+    }
+
+
+
+    private static void concurrency() throws Exception {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(() -> {
+            int a = 0;
+            for (long i = 0; i < count; i++) {
+                a += 5;
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + "ms, b=" + b);
+    }
+}
