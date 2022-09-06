a44b0d20fa2f889bbb39c7d84662df0ea013ab9b
liaoqingsheng <2677388021@qq.com>
2022-09-06 11:57:15 +0800

    第一次提交

--- src/main/java/com/interesting/modules/MyTest.java
+++ src/main/java/com/interesting/modules/MyTest.java
@@ -5,9 +5,6 @@ public class MyTest {
         System.out.println(1);
         System.out.println(2);
         System.out.println(123);
-<<<<<<<<< Temporary merge branch 1
-=========
-        System.out.println(456);
->>>>>>>>> Temporary merge branch 2
+        System.out.println(123);
     }
 }
