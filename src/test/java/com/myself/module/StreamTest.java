9dd31a846c827604296c3650d0831f2e5e4fa2c0
liaoqingsheng <2677388021@qq.com>
2022-09-06 11:49:41 +0800

    feat(modules模块): 简单描述
    -长描述
    -东阿发撒扥

---
+++ src/test/java/com/myself/module/StreamTest.java
@@ -0,0 +1,11 @@
+package com.myself.module;
+
+public class StreamTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(2);
+
+    }
+}
