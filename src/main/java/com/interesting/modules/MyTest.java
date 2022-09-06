c25e313bce14721db527ad672e9ec579cd1595fd
liaoqingsheng <2677388021@qq.com>
2022-09-06 11:54:18 +0800

    fix(modules模块): 简单描述
    -长描述
    -东阿发撒扥

--- src/main/java/com/interesting/modules/MyTest.java
+++ src/main/java/com/interesting/modules/MyTest.java
@@ -4,5 +4,7 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(123);
+        System.out.println(456);
     }
 }
