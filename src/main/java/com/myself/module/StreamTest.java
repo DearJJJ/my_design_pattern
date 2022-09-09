1ee892c0110b8fbf7f968d8f0bbcd14da3eee0b3
liaoqingsheng <2677388021@qq.com>
2022-09-09 08:54:01 +0800

    本地提交1

--- src/main/java/com/myself/module/StreamTest.java
+++ src/main/java/com/myself/module/StreamTest.java
@@ -3,5 +3,7 @@ package com.myself.module;
 public class StreamTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println(1);
     }
 }
